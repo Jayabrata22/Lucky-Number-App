36adf6fca4189cccd6e76bd20d6b1e56b1a2b183
Jayabrata Das <jayabrata.das@ad.infosys.com>
2022-12-14 15:49:24 +0530

    Add project files.

---
+++ LuckyNumberApp/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Support.V4.App;
+using System;
+
+namespace LuckyNumberApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        SeekBar seekBar;
+        TextView resultTextView;
+        Button pushButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Set our view from the "main" layout resour ce
+            SetContentView(Resource.Layout.activity_main);
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
+            pushButton = (Button)FindViewById(Resource.Id.pushButton);
+            pushButton.Click += PushButton_Click;
+        }
+
+        private void PushButton_Click(object sender, System.EventArgs e)
+        {
+            Random random = new Random();
+            int LuckyNumber = random.Next(seekBar.Progress) + 1;
+            resultTextView.Text = LuckyNumber.ToString();
+        }
+    }
+}
